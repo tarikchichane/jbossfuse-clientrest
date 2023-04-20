5907c9c3e1aba6fe64da6635d7f01ffcd6a58906
tarik chichane <tarikchichane2007@gmail.com>
2023-04-20 01:12:42 +0000

    Add File

---
+++ src/main/java/org/bam/rest/consumer/camelrestconsumer/CamelrestconsumerApplication.java
@@ -0,0 +1,13 @@
+package org.bam.rest.consumer.camelrestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelrestconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelrestconsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bam/rest/consumer/camelrestconsumer/CamelrestconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bam.rest.consumer.camelrestconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelrestconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
